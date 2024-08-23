61407a10f35084424c4d3757fd4b9c252bfae261
Mohamedshaban96 <mohamedshaban1234@gmail.com>
2024-08-22 19:22:24 -0700

    task1

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,8 @@
+# enables the user holberton to login and open files without error
+# Increase hard file limit
+
+
+exec {'OS security config':
+  command => 'sed -i "s/holberton/foo/" /etc/security/limits.conf',
+  path    => '/usr/bin/env/:/bin/:/usr/bin/:/usr/sbin/'
+}
